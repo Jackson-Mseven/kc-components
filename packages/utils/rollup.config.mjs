5895f43e5fce626b29fe10c941bb0dd471839cf6
kincy <bill714@foxmail.com>
2025-03-31 15:43:10 +0800

    fix: 修改错误的 defineConfig 导入

--- packages/utils/rollup.config.mjs
+++ packages/utils/rollup.config.mjs
@@ -1,7 +1,7 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import { defineConfig } from 'dumi';
+import { defineConfig } from 'rollup';
 import { terser } from 'rollup-plugin-terser';
 
 const DIR_MAP = {
